4091e4a145dce0784079e97967cf6ec0ca3aaff5
KauaCorreaArruda <kaua.correa.arruda@gmail.com>
2025-06-09 20:53:36 -0300

    initial commit

---
+++ src/test/java/com/github/kauacorreaarruda/web_services/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kauacorreaarruda.web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
