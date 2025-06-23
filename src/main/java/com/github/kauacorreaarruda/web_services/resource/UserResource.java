18000dca56e59d7901b6792ca5b5dfec7a7cff84
KauaCorreaArruda <kaua.correa.arruda@gmail.com>
2025-06-23 18:31:26 -0300

    exception handling - findById

--- src/main/java/com/github/kauacorreaarruda/web_services/resource/UserResource.java
+++ src/main/java/com/github/kauacorreaarruda/web_services/resource/UserResource.java
@@ -3,7 +3,6 @@ package com.github.kauacorreaarruda.web_services.resource;
 import com.github.kauacorreaarruda.web_services.entity.User;
 import com.github.kauacorreaarruda.web_services.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
